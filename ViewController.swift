2945cfc9189ab55bd28e553645598e32381fd2a2
viktoryav <viktoryav@yahoo.com>
2014-11-08 12:34:44 +0000

    initial version

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiplication
+//
+//  Created by Vika on 08/11/2014.
+//  Copyright (c) 2014 Vika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
