669ca4e3afb2317b5d321940c45494bf1f91bbe0
viktoryav <viktoryav@yahoo.com>
2014-11-10 10:13:38 +0000

    add main, multiplication main, division main

---
+++ TimeTablesData.swift
@@ -0,0 +1,70 @@
+//
+//  TimeTablesData.swift
+//  Navigation
+//
+//  Created by Vika on 04/11/2014.
+//  Copyright (c) 2014 Vika. All rights reserved.
+//
+
+import Foundation
+
+class TimeTablesData {
+    
+    let one = "1"
+    let two = "2"
+    let three = "3"
+    let four = "4"
+    let five = "5"
+    let six = "6"
+    let seven = "7"
+    let eight = "8"
+    let nine = "9"
+    let ten = "10"
+    
+    var knownTimeTables = [String]()
+    var currentlyLearningTimeTable:String?
+    var allTimeTables = [String]()
+    
+    init()
+    {
+        initTimeTables()
+    }
+    
+    func initTimeTables()
+    {
+        allTimeTables.append(one)
+        allTimeTables.append(two)
+        allTimeTables.append(three)
+        allTimeTables.append(four)
+        allTimeTables.append(five)
+        allTimeTables.append(six)
+        allTimeTables.append(seven)
+        allTimeTables.append(eight)
+        allTimeTables.append(nine)
+        allTimeTables.append(ten)
+    }
+    
+    func getUnknownTimeTables() -> Array<String>
+    {
+        var unknownTimeTables = [String]()
+        for index_all in allTimeTables
+        {
+            var hasIndex = false
+            for index in knownTimeTables
+            {
+                if(index == index_all)
+                {
+                    hasIndex = true
+                    break
+                }
+            }
+            if(!hasIndex)
+            {
+                unknownTimeTables.append(index_all)
+                println(index_all)
+            }
+        }
+        return unknownTimeTables
+    }
+    
+}
--- ViewController.swift
+++ ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var multiplicationButton: UIButton!
+    
+    @IBOutlet weak var divisionButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
